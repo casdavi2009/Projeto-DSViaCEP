9fe89f84fb44101770acce7309602398d1ad7710
Davi Castro Silva <silva.davicastro@hotmail.com>
2025-03-15 19:09:46 -0300

    Seleção dos elementos do form e criação da função address

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,7 +1,33 @@
+import Address from '../models/address.js';
 
+function State(){
 
+    this.address = new Address();
+
+    this.btnSave = null;
+    this.btnClear = null;
+    
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.errorNumber = null
+}
+const state = new State();
 
 export function init(){
-    const inputCep = document.forms.newAdress.cep;
-    console.log(inputCep);
+    state.inputCep = document.forms.newAdress.cep;
+    state.inputStreet = document.forms.newAdress.street;
+    state.inputNumber = document.forms.newAdress.number;
+    state.inputCity = document.forms.newAdress.city;
+
+    state.btnSave = document.forms.newAdress.btnSave;
+    state.btnClear = document.forms.newAdress.btnClear;
+
+    state.errorCep = document.querySelector('[data-error="cep"]');
+    state.errorNumber = document.querySelector('[data-error="number"]');
+
+    console.log(state);
 }
