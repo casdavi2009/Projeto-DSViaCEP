90f95d4c120c70ca58f2a45653ad53646a185172
Davi Castro Silva <silva.davicastro@hotmail.com>
2025-04-11 12:39:14 -0300

    Verificação do objeto Address antes de salvar

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -62,9 +62,20 @@ async function handleInputCepChange(event){
     }
 }
 
-async function handleBtnSaveClick(event){
+function handleBtnSaveClick(event){
     event.preventDefault();
-    listController.addCard(state.address);
+    const errors = addressService.getErrors(state.address);
+    const keys = Object.keys(errors);
+    
+    if (keys.length > 0){
+        keys.forEach(key => {
+            setFormError(key, errors[key]);
+        });
+    }
+    else {
+        listController.addCard(state.address);
+        clearForm();
+    }
 }
 
 function handleBtnClearClick(event){
@@ -81,6 +92,7 @@ function clearForm(){
     setFormError("cep", "");
     setFormError("number", "");
 
+    state.address = new Address();
     state.inputCep.focus();
 }
 
